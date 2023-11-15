6ccbb02dac9c87a432d3996c3848b4bba3e97709
jocelyn <jocelynwlt2026@student.cis.edu>
2023-11-15 19:27:16 +0800

    in progress

--- src/ArrayUtility.java
+++ src/ArrayUtility.java
@@ -1,34 +1,73 @@
+import java.text.DecimalFormat;
+import java.util.Arrays;
+
 public class ArrayUtility {
 
     public static void print(int[] array) {
-
+        System.out.print(Arrays.toString(array));
     }
 
     public static int sum(int[] array) {
-        return 0;
+        int output = 0;
+        int len = array.length;
+        for (int i : array) {
+            output += i;
+        }
+        return output;
     }
 
     public static double average(int[] array) {
-        return 0;
+        int add = 0;
+        int len = array.length;
+        for (int i : array) {
+            add += i;
+        }
+        double dadd = (int)add;
+        double lent = (int)len;
+        double output = dadd/len;
+        return output;
     }
 
     public static int minimum(int[] array) {
-        return 0;
+        int len = array.length;
+        int num = array[0];
+        for(int i = 0; i<len; i++){
+            if(array[i]<num){
+                num = array[i];
+            }
+        }
+        return num;
+        
     }
 
     public static int maximum(int[] array) {
-        return 0;
+        int len = array.length;
+        int num = array[0];
+        for(int i = 0; i<len; i++){
+            if(array[i]>num){
+                num = array[i];
+            }
+        }
+        return num;
     }
 
     public static int evenCount(int[] array) {
         return 0;
     }
 
-    public static int[] reverseOne(int[] array) {
-        return null;
+    public static int[] reverseOne(int[] array){
+        int len = array.length;
+        int[] output = new int[len];
+        for (int i = 0; i<len; i++){
+            output[i]=array[len-1-i];
+        }
+        return output;
     }
 
+
+
     public static void reverseTwo(int[] array) {
+
 
     }
 
